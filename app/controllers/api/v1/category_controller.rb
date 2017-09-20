676e2143965750e0a15cfb8ff1264a2a14f9d063
Yaofeng Yue <yyue@neat.com>
2017-09-20 11:45:36 -0400

    switched to use json_api as model serialier adapter

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::CategoryController < ApplicationController
   def show
-    category = params[:category] ? params[:category] : "sports"
+    category = params[:category] ? params[:category] : "Main_topic_classifications"
     @category = Category.where(cat_title: category.capitalize).first
-    render json: @category
-    #render json: @category, serializer: CategorySerializer, root: "category"
+    render json: @category, serializer: CategorySerializer
   end
 end
--- app/controllers/api/v1/graph_controller.rb
+++ app/controllers/api/v1/graph_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::GraphController < ApplicationController
   def show
     cat = params[:category] || 'sports'
     @category = Category.where(:cat_title => cat.capitalize).first
-    @links = Link.where(:cl_to => @category.cat_title,
-                        :cl_type => "subcat")
-    render :json => @links, each_serializer: LinkSerializer
+    if @category
+      @links = Link.where(:cl_to => @category.cat_title,
+                          :cl_type => "subcat")
+      render :json => @links, each_serializer: LinkSerializer
+    else
+      render :json => {:error => {:text => "404 Not Found", :status => 404}}
+    end
   end
   private
   def graph_params
